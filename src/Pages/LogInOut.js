8925cfe87a692d50055d516ea9016d6c7906cf09
- <118478769+SasankaHansajith@users.noreply.github.com>
2025-02-20 14:21:16 +0530

    First commit

---
+++ src/Pages/LogInOut.js
@@ -0,0 +1,9 @@
+import LogInOut from "./Pages/LogInOut";
+import React from "react";
+import "./Background.css"; // Import CSS for styling
+
+const Background = () => {
+  return <div className="background"></div>;
+};
+
+export default Background;
